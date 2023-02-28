b59074ea8b4f545c4cbc224129ea9679ba9565f8
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2023-02-28 15:33:41 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="skeleton-methods",
-    version="0.0.2",
+    version="0.0.4",
     author="Zeyu Wei, Yikun Zhang",
     author_email="zwei5@uw.edu, yikunzhang@foxmail.com",
     description="Skeleton-Based Methods for Clustering and Regression on Underlying Manifolds",
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(include=["skeleton-methods"]),
+    packages=setuptools.find_packages(include=["skeleton_methods"]),
     install_requires=["numpy", "scipy", "scikit-learn", "igraph"],
     python_requires=">=3.6",
 )
